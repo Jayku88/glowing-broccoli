9d54fb8ce83463a0e1d30368a7e86148ed9546d8
Jayku88 <45588382+Jayku88@users.noreply.github.com>
2025-07-07 20:20:44 +0530

    Update runapp.py

--- runapp.py
+++ runapp.py
@@ -25,7 +25,7 @@ if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
-    st.image(image, use_column_width=True)
+    st.image(image, use_container_width=True)
     prediction = import_and_predict(image, model)
 
     if np.argmax(prediction) == 0:
