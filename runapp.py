07a52fd29491654786179ab7ee70a706f3ce18fc
Jayku88 <45588382+Jayku88@users.noreply.github.com>
2025-07-08 09:56:23 +0530

    Update runapp.py

--- runapp.py
+++ runapp.py
@@ -62,12 +62,6 @@ st.write("Upload a satellite image and the model will extract road regions.")
 # File uploader
 file = st.file_uploader("Please upload a satellite image", type=["jpg", "png", "jpeg"])
 
-# Image preprocessing + prediction
-# def preprocess_image(image_data, target_size=(512, 512)):
-#     image_array = np.array(image_data)           # Convert to NumPy array first
-#     image_array = image_array / 255.0 
-#     return image_array
-
 def preprocess_image(image_data, target_size=(512, 512)):
     # Ensure image is RGB
     image_data = image_data.convert('RGB')
@@ -78,31 +72,20 @@ def preprocess_image(image_data, target_size=(512, 512)):
     # Convert to NumPy array and normalize
     image_array = np.array(image_data).astype(np.float32) / 255.0  # (512, 512, 3)
     image_array = np.expand_dims(image_array, 0)
-    # image_array = np.array(image_data / 255.0 )
-    # image_array = np.expand_dims(image_array, 0)
     return image_array
 
 if file is not None:
     image = Image.open(file).convert("RGB")
     st.image(image, caption='Uploaded Image', use_container_width=True)
-
     input_image = preprocess_image(image)
-    
-    # prediction = model.predict(input_image)
-    # predictions = (prediction > 0.1).astype(np.uint8)
-
     prediction = model.predict(input_image)
-    #predictions = (prediction > 0.5).astype(np.uint8)
+    
     
     print(prediction.shape)
     mask = prediction.squeeze()          # shape: (512, 512)
     print(mask.shape)
     mask = (mask * 255).astype(np.uint8)  # scale binary to [0, 255]
     mask_image = Image.fromarray(mask)
-
-    #mask = postprocess_mask(prediction)
-
-    #st.image(predictions, caption='Predicted Road Mask', use_container_width=True, clamp=True)
     st.image(prediction, caption='Predicted Road Mask', use_container_width=True)
 
 else:
